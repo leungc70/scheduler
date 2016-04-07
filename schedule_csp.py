41ea2627faad125c2229f7bf78d26991e9e15708
Ryan <leung.ryan.ca@gmail.com>
2016-04-07 00:02:53 -0400

    updated get_commute_time parameters

--- schedule_csp.py
+++ schedule_csp.py
@@ -102,8 +102,11 @@ def schedule_csp_model(profs, students, time_frame):
         
     return schedule_csp,var_array
 
-def get_commute_time(loc1,loc2,distance):
-    d = distance[(loc1,loc2)]
+def get_commute_time(prof1,prof2,distance,loaction):
+    prof1_loc = loaction[prof1][0]
+    prof2_loc = loaction[prof2][0]
+    d = distance[(prof1_loc,prof2_loc)]
+    print(d)
     if d < 800:
         return 0
     else:
@@ -123,8 +126,17 @@ def print_soln(var_array):
         
         print("{} = {} to {}".format(var,start,end))
 
-
-
+def print_table(var_array):
+    print(len(var_array))
+    print("         | 9am to 10am | 10am to 11am | 11am to 12pm | \
+12pm to  1pm | 1pm to 2pm | 2pm to 3pm | 3pm to 4pm | 4pm to 5pm |")
+    p = set()
+    for v in var_array:
+        p.add(v.prof_name)
+    
+    for n in p:
+        print(n)
+        
 if __name__ == '__main__':
     
     # 0 to 24 hours
@@ -180,3 +192,5 @@ if __name__ == '__main__':
     print()
     print("=====Solution=====")
     print_soln(var_array)
+    print_table(var_array)
+   
